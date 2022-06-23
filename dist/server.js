fe7f4c7f6177145da6d07e99007c755fbf58c154
juratbekoff <juratbekxudayberganov9@gmail.com>
2022-06-23 15:11:18 +0300

    mycommits

--- dist/server.js
+++ dist/server.js
@@ -7,7 +7,45 @@ var express_1 = __importDefault(require("express"));
 var cors_1 = __importDefault(require("cors"));
 var message_routes_1 = __importDefault(require("./routes/message.routes"));
 var publish_routes_1 = __importDefault(require("./routes/publish.routes"));
+//Swagger
+var swagger_jsdoc_1 = __importDefault(require("swagger-jsdoc"));
+var swaggerDefinition = {
+    openapi: '3.0.0',
+    info: {
+        title: 'Express API for 5P Marekting Agency',
+        version: '1.0.0',
+        description: 'This is a REST API application made with Express. It retrieves data from JSONPlaceholder.',
+        contact: {
+            name: 'JSONPlaceholder',
+            url: 'https://jsonplaceholder.typicode.com'
+        }
+    },
+    servers: [
+        {
+            url: 'http://localhost:8085',
+            description: 'Main Development Server!'
+        },
+    ]
+};
+var options = {
+    swaggerDefinition: swaggerDefinition,
+    // Paths to files containing OpenAPI definitions
+    apis: ['./routes/*.js']
+};
+var swaggerSpec = (0, swagger_jsdoc_1["default"])(options);
+// server.js
+// ...
+var swagger_ui_express_1 = __importDefault(require("swagger-ui-express"));
+// ...
 var app = (0, express_1["default"])();
+app.use('/docs', swagger_ui_express_1["default"].serve, swagger_ui_express_1["default"].setup(swaggerSpec));
+/**
+ * @swagger
+ * /users:
+ *   get:
+ *     summary: Retrieve a list of JSONPlaceholder users
+ *     description: Retrieve a list of users from JSONPlaceholder. Can be used to populate a list of fake users when prototyping or testing an API.
+*/
 app.use((0, cors_1["default"])());
 app.use(express_1["default"].json());
 app.use(express_1["default"].urlencoded({ extended: true }));
--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,54 @@ import cors from 'cors'
 import messageRoutes from "./routes/message.routes";
 import publishRoutes from "./routes/publish.routes"
 
+//Swagger
+import swaggerJSDoc from 'swagger-jsdoc'
+
+const swaggerDefinition = {
+    openapi: '3.0.0',
+    info: {
+      title: 'Express API for 5P Marekting Agency',
+      version: '1.0.0',
+      description:
+        'This is a REST API application made with Express. It retrieves data from JSONPlaceholder.',
+      contact: {
+        name: 'JSONPlaceholder',
+        url: 'https://jsonplaceholder.typicode.com',
+      },
+    },
+    servers: [
+      {
+        url: 'http://localhost:8085',
+        description: 'Main Development Server!',
+      },
+    ],
+  };
+
+const options = {
+    swaggerDefinition,
+    // Paths to files containing OpenAPI definitions
+    apis: ['./routes/*.js'],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+// server.js
+// ...
+import swaggerUi from 'swagger-ui-express'
+// ...
+
 const app = express()
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+/**
+ * @swagger
+ * /users:
+ *   get:
+ *     summary: Retrieve a list of JSONPlaceholder users
+ *     description: Retrieve a list of users from JSONPlaceholder. Can be used to populate a list of fake users when prototyping or testing an API.
+*/
+
 
 app.use(cors())
 app.use(express.json())
